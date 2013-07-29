c947af2fe70614bc947a77dfe4aa9eb897cd55e2
PavelGnatyuk <pgnatyuk@gmail.com>
2013-07-29 15:27:27 +0300

    Candle Fire Project

---
+++ CandleFire/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CandleFire
+//
+//  Created by Pavel Gnatyuk on 7/29/13.
+//  Copyright (c) 2013 Pavel Gnatyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
