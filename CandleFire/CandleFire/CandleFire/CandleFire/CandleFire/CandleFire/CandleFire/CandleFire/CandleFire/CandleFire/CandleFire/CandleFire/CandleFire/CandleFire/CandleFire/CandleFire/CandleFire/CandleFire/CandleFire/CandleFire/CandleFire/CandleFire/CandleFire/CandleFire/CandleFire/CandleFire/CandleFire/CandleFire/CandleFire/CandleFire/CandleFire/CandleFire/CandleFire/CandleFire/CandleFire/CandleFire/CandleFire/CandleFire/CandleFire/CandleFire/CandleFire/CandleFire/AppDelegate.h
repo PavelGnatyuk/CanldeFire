8e9e9eee46e72f04ea91a35e0c5f33cfbd120296
PavelGnatyuk <pgnatyuk@gmail.com>
2013-07-29 15:39:35 +0300

    resources

---
+++ CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/CandleFire/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CandleFire
+//
+//  Created by Pavel Gnatyuk on 7/29/13.
+//  Copyright (c) 2013 Pavel Gnatyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
